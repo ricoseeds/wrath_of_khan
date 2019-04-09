2a4306eba988a5d9730d1db0001adff77ce045c1
ricoseeds <rghchaks73@gmail.com>
2019-04-09 16:51:55 +0530

    save

---
+++ include/main.h
@@ -0,0 +1,241 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#define GLEW_STATIC
+#include "GL/glew.h" // Important - this header must come before glfw3 header
+#include "GLFW/glfw3.h"
+#define GLM_ENABLE_EXPERIMENTAL
+#define GLM_FORCE_RADIANS
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+#include "glm/gtx/string_cast.hpp"
+#include "../include/ShaderProgram.h"
+#include "../include/Texture2D.h"
+#include "../include/Camera.h"
+#include "../include/Mesh.h"
+
+
+// Global Variables
+const char *APP_TITLE = "Introduction to Modern OpenGL - Multiple Lights";
+int gWindowWidth = 1024;
+int gWindowHeight = 768;
+GLFWwindow *gWindow = NULL;
+bool gWireframe = false;
+bool gFlashlightOn = true;
+glm::vec4 gClearColor(0.06f, 0.06f, 0.07f, 1.0f);
+static bool mac_moved = false;
+
+FPSCamera fpsCamera(glm::vec3(0.0f, 3.5f, 10.0f));
+const double ZOOM_SENSITIVITY = -3.0;
+const float MOVE_SPEED = 5.0; // units per second
+const float MOUSE_SENSITIVITY = 0.1f;
+
+// Function prototypes
+void glfw_onKey(GLFWwindow *window, int key, int scancode, int action, int mode);
+void glfw_onFramebufferSize(GLFWwindow *window, int width, int height);
+void glfw_onMouseScroll(GLFWwindow *window, double deltaX, double deltaY);
+void update(double elapsedTime);
+void showFPS(GLFWwindow *window);
+bool initOpenGL();
+float rand_num(int LO, int HI);
+void mac_patch(GLFWwindow *window);
+//-----------------------------------------------------------------------------
+// Initialize GLFW and OpenGL
+//-----------------------------------------------------------------------------
+bool initOpenGL()
+{
+	// Intialize GLFW
+	// GLFW is configured.  Must be called before calling any GLFW functions
+	if (!glfwInit())
+	{
+		// An error occured
+		std::cerr << "GLFW initialization failed" << std::endl;
+		return false;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // forward compatible with newer versions of OpenGL as they become available but not backward compatible (it will not run on devices that do not support OpenGL 3.3
+
+	// Create an OpenGL 3.3 core, forward compatible context window
+	gWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+	if (gWindow == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return false;
+	}
+
+	// Make the window's context the current one
+	glfwMakeContextCurrent(gWindow);
+
+	// Initialize GLEW
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Failed to initialize GLEW" << std::endl;
+		return false;
+	}
+
+	// Set the required callback functions
+	glfwSetKeyCallback(gWindow, glfw_onKey);
+	glfwSetFramebufferSizeCallback(gWindow, glfw_onFramebufferSize);
+	glfwSetScrollCallback(gWindow, glfw_onMouseScroll);
+
+	// // Hides and grabs cursor, unlimited movement
+	// glfwSetInputMode(gWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	// glfwSetCursorPos(gWindow, gWindowWidth / 2.0, gWindowHeight / 2.0);
+
+	glClearColor(gClearColor.r, gClearColor.g, gClearColor.b, gClearColor.a);
+
+	// Define the viewport dimensions
+	glViewport(0, 0, gWindowWidth, gWindowHeight);
+	glEnable(GL_DEPTH_TEST);
+
+	return true;
+}
+
+//-----------------------------------------------------------------------------
+// Is called whenever a key is pressed/released via GLFW
+//-----------------------------------------------------------------------------
+void glfw_onKey(GLFWwindow *window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (key == GLFW_KEY_F1 && action == GLFW_PRESS)
+	{
+		gWireframe = !gWireframe;
+		if (gWireframe)
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		else
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+
+	if (key == GLFW_KEY_F && action == GLFW_PRESS)
+	{
+		// toggle the flashlight
+		gFlashlightOn = !gFlashlightOn;
+	}
+}
+
+//-----------------------------------------------------------------------------
+// Is called when the window is resized
+//-----------------------------------------------------------------------------
+void glfw_onFramebufferSize(GLFWwindow *window, int width, int height)
+{
+	gWindowWidth = width;
+	gWindowHeight = height;
+	glViewport(0, 0, gWindowWidth, gWindowHeight);
+}
+
+//-----------------------------------------------------------------------------
+// Called by GLFW when the mouse wheel is rotated
+//-----------------------------------------------------------------------------
+void glfw_onMouseScroll(GLFWwindow *window, double deltaX, double deltaY)
+{
+	double fov = fpsCamera.getFOV() + deltaY * ZOOM_SENSITIVITY;
+
+	fov = glm::clamp(fov, 1.0, 120.0);
+
+	fpsCamera.setFOV((float)fov);
+}
+
+//-----------------------------------------------------------------------------
+// Update stuff every frame
+//-----------------------------------------------------------------------------
+void update(double elapsedTime)
+{
+	// Camera orientation
+	double mouseX, mouseY;
+
+	// Get the current mouse cursor position delta
+	// glfwGetCursorPos(gWindow, &mouseX, &mouseY);
+
+	// // Rotate the camera the difference in mouse distance from the center screen.  Multiply this delta by a speed scaler
+	// fpsCamera.rotate((float)(gWindowWidth / 2.0 - mouseX) * MOUSE_SENSITIVITY, (float)(gWindowHeight / 2.0 - mouseY) * MOUSE_SENSITIVITY);
+
+	// // Clamp mouse cursor to center of screen
+	// glfwSetCursorPos(gWindow, gWindowWidth / 2.0, gWindowHeight / 2.0);
+
+	// Camera FPS movement
+
+	// Forward/backward
+	if (glfwGetKey(gWindow, GLFW_KEY_W) == GLFW_PRESS)
+		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * fpsCamera.getLook());
+	else if (glfwGetKey(gWindow, GLFW_KEY_S) == GLFW_PRESS)
+		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * -fpsCamera.getLook());
+
+	// Strafe left/right
+	if (glfwGetKey(gWindow, GLFW_KEY_A) == GLFW_PRESS)
+		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * -fpsCamera.getRight());
+	else if (glfwGetKey(gWindow, GLFW_KEY_D) == GLFW_PRESS)
+		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * fpsCamera.getRight());
+
+	// Up/down
+	if (glfwGetKey(gWindow, GLFW_KEY_Z) == GLFW_PRESS)
+		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * glm::vec3(0.0f, 1.0f, 0.0f));
+	else if (glfwGetKey(gWindow, GLFW_KEY_X) == GLFW_PRESS)
+		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * -glm::vec3(0.0f, 1.0f, 0.0f));
+}
+
+//-----------------------------------------------------------------------------
+// Code computes the average frames per second, and also the average time it takes
+// to render one frame.  These stats are appended to the window caption bar.
+//-----------------------------------------------------------------------------
+void showFPS(GLFWwindow *window)
+{
+	static double previousSeconds = 0.0;
+	static int frameCount = 0;
+	double elapsedSeconds;
+	double currentSeconds = glfwGetTime(); // returns number of seconds since GLFW started, as double float
+
+	elapsedSeconds = currentSeconds - previousSeconds;
+
+	// Limit text updates to 4 times per second
+	if (elapsedSeconds > 0.25)
+	{
+		previousSeconds = currentSeconds;
+		double fps = (double)frameCount / elapsedSeconds;
+		double msPerFrame = 1000.0 / fps;
+
+		// The C++ way of setting the window title
+		std::ostringstream outs;
+		outs.precision(3); // decimal places
+		outs << std::fixed
+			 << APP_TITLE << "    "
+			 << "FPS: " << fps << "    "
+			 << "Frame Time: " << msPerFrame << " (ms)";
+		glfwSetWindowTitle(window, outs.str().c_str());
+
+		// Reset for next average.
+		frameCount = 0;
+	}
+
+	frameCount++;
+}
+void mac_patch(GLFWwindow *window)
+{
+	if (glfwGetTime() > 3.0)
+	{
+		mac_moved = true;
+	}
+	// glfwGetTim
+
+	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_RELEASE && !mac_moved)
+	{
+		int x, y;
+		glfwGetWindowPos(window, &x, &y);
+		glfwSetWindowPos(window, ++x, y);
+	}
+	else
+	{
+		mac_moved = true;
+	}
+}
+
+float rand_num(int LO, int HI){
+	return (LO + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(HI-LO))));
+
+}
--- src/main.cpp
+++ src/main.cpp
@@ -1,42 +1,7 @@
-#include <iostream>
-#include <sstream>
-#include <string>
-#define GLEW_STATIC
-#include "GL/glew.h" // Important - this header must come before glfw3 header
-#include "GLFW/glfw3.h"
-#define GLM_ENABLE_EXPERIMENTAL
-#define GLM_FORCE_RADIANS
-#include "glm/glm.hpp"
-#include "glm/gtc/matrix_transform.hpp"
-#include "glm/gtx/string_cast.hpp"
-#include "../include/ShaderProgram.h"
-#include "../include/Texture2D.h"
-#include "../include/Camera.h"
-#include "../include/Mesh.h"
 
-// Global Variables
-const char *APP_TITLE = "Introduction to Modern OpenGL - Multiple Lights";
-int gWindowWidth = 1024;
-int gWindowHeight = 768;
-GLFWwindow *gWindow = NULL;
-bool gWireframe = false;
-bool gFlashlightOn = true;
-glm::vec4 gClearColor(0.06f, 0.06f, 0.07f, 1.0f);
-static bool mac_moved = false;
+#include "../include/main.h"
 
-FPSCamera fpsCamera(glm::vec3(0.0f, 3.5f, 10.0f));
-const double ZOOM_SENSITIVITY = -3.0;
-const float MOVE_SPEED = 5.0; // units per second
-const float MOUSE_SENSITIVITY = 0.1f;
 
-// Function prototypes
-void glfw_onKey(GLFWwindow *window, int key, int scancode, int action, int mode);
-void glfw_onFramebufferSize(GLFWwindow *window, int width, int height);
-void glfw_onMouseScroll(GLFWwindow *window, double deltaX, double deltaY);
-void update(double elapsedTime);
-void showFPS(GLFWwindow *window);
-bool initOpenGL();
-void mac_patch(GLFWwindow *window);
 //-----------------------------------------------------------------------------
 // Main Application Entry Point
 //-----------------------------------------------------------------------------
@@ -58,11 +23,19 @@ int main()
 	const int numModels = 1;
 	Mesh mesh[numModels];
 	Texture2D texture[numModels];
-	int no_particles = 1;
+	int no_particles = 20;
 	Mesh mlights[no_particles];
 
 	mesh[0].loadOBJ("models/Earth_Triangulated.obj");
-	mlights[0].loadOBJ("models/square.obj");
+
+	std::vector<glm::vec3> light_pos;
+	for(int i = 0; i < no_particles; i++ ){
+		mlights[i].loadOBJ("models/square.obj");
+		light_pos.push_back(glm::vec3(rand() % 10, 0.0 , 0.0));
+		std::cout << glm::to_string(light_pos[i]);
+	}
+	// mlights[0].loadOBJ("models/square.obj");
+	// mlights[1].loadOBJ("models/square.obj");
 
 	texture[0].loadTexture("textures/Earth_TEXTURE_CM.tga", true);
 	// texture[1].loadTexture("textures/light_diffuse.jpg", true);
@@ -129,7 +102,7 @@ int main()
 		lightingShader.setUniform("sunLight.ambient", glm::vec3(0.1f, 0.1f, 0.1f));
 		lightingShader.setUniform("sunLight.diffuse", glm::vec3(0.1f, 0.1f, 0.1f)); // dark
 		lightingShader.setUniform("sunLight.specular", glm::vec3(0.1f, 0.1f, 0.1f));
-
+		// std::cout << rand_num(10, 20) << std::endl;
 		// // Point Light 1
 		// lightingShader.setUniform("pointLights[0].ambient", glm::vec3(0.2f, 0.2f, 0.2f));
 		// lightingShader.setUniform("pointLights[0].diffuse", glm::vec3(0.0f, 1.0f, 0.1f)); // green-ish light
@@ -139,28 +112,11 @@ int main()
 		// lightingShader.setUniform("pointLights[0].linear", 0.0022f);
 		// lightingShader.setUniform("pointLights[0].exponent", 0.0020f);
 
-		// // Point Light 2
-		// lightingShader.setUniform("pointLights[1].ambient", glm::vec3(0.2f, 0.2f, 0.2f));
-		// lightingShader.setUniform("pointLights[1].diffuse", glm::vec3(1.0f, 0.1f, 0.0f)); // red-ish light
-		// lightingShader.setUniform("pointLights[1].specular", glm::vec3(1.0f, 1.0f, 1.0f));
-		// lightingShader.setUniform("pointLights[1].position", pointLightPos[1]);
-		// lightingShader.setUniform("pointLights[1].constant", 1.0f);
-		// lightingShader.setUniform("pointLights[1].linear", 0.0022f);
-		// lightingShader.setUniform("pointLights[1].exponent", 0.0020f);
-
-		// // Point Light 3
-		// lightingShader.setUniform("pointLights[2].ambient", glm::vec3(0.2f, 0.2f, 0.2f));
-		// lightingShader.setUniform("pointLights[2].diffuse", glm::vec3(0.0f, 0.1f, 1.0f)); // blue-ish light
-		// lightingShader.setUniform("pointLights[2].specular", glm::vec3(1.0f, 1.0f, 1.0f));
-		// lightingShader.setUniform("pointLights[2].position", pointLightPos[2]);
-		// lightingShader.setUniform("pointLights[2].constant", 1.0f);
-		// lightingShader.setUniform("pointLights[2].linear", 0.0022f);
-		// lightingShader.setUniform("pointLights[2].exponent", 0.0020f);
 
 		// Spot light
 		glm::vec3 spotlightPos = fpsCamera.getPosition();
-		std::cout << "POS : " << glm::to_string(fpsCamera.getPosition()) << std::endl;
-		std::cout << "LOOK : " << glm::to_string(fpsCamera.getLook()) << std::endl;
+		// std::cout << "POS : " << glm::to_string(fpsCamera.getPosition()) << std::endl;
+		// std::cout << "LOOK : " << glm::to_string(fpsCamera.getLook()) << std::endl;
 		// offset the flash light down a little
 		spotlightPos.y -= 0.5f;
 
@@ -201,12 +157,16 @@ int main()
 			mesh[i].draw();		// Render the OBJ mesh
 			texture[i].unbind(0);
 		}
-		bulb.use();
-        bulb.setUniform("lightColor", glm::vec3(1.0f, 1.0f, 0.0f));
-        bulb.setUniform("model", glm::translate(glm::mat4(1.0), glm::vec3(4.0f, 0.0f, 0.0f)));
-        bulb.setUniform("view", view);
-        bulb.setUniform("projection", projection);
-        mlights[0].draw();
+
+		for(int i =0; i< no_particles; i++){
+			bulb.use();
+			bulb.setUniform("lightColor", glm::vec3(1.0f, 1.0f, 0.0f));
+			bulb.setUniform("model", glm::translate(glm::mat4(1.0), light_pos[i]) * glm::scale(glm::mat4(1.0), glm::vec3(0.02, 0.02, 0.02)));
+			bulb.setUniform("view", view);
+			bulb.setUniform("projection", projection);
+			mlights[0].draw();
+		}
+
 
 		// Swap front and back buffers
 		glfwSwapBuffers(gWindow);
@@ -219,198 +179,203 @@ int main()
 	return 0;
 }
 
-//-----------------------------------------------------------------------------
-// Initialize GLFW and OpenGL
-//-----------------------------------------------------------------------------
-bool initOpenGL()
-{
-	// Intialize GLFW
-	// GLFW is configured.  Must be called before calling any GLFW functions
-	if (!glfwInit())
-	{
-		// An error occured
-		std::cerr << "GLFW initialization failed" << std::endl;
-		return false;
-	}
+// //-----------------------------------------------------------------------------
+// // Initialize GLFW and OpenGL
+// //-----------------------------------------------------------------------------
+// bool initOpenGL()
+// {
+// 	// Intialize GLFW
+// 	// GLFW is configured.  Must be called before calling any GLFW functions
+// 	if (!glfwInit())
+// 	{
+// 		// An error occured
+// 		std::cerr << "GLFW initialization failed" << std::endl;
+// 		return false;
+// 	}
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // forward compatible with newer versions of OpenGL as they become available but not backward compatible (it will not run on devices that do not support OpenGL 3.3
+// 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+// 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+// 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+// 	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // forward compatible with newer versions of OpenGL as they become available but not backward compatible (it will not run on devices that do not support OpenGL 3.3
 
-	// Create an OpenGL 3.3 core, forward compatible context window
-	gWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
-	if (gWindow == NULL)
-	{
-		std::cerr << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return false;
-	}
+// 	// Create an OpenGL 3.3 core, forward compatible context window
+// 	gWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+// 	if (gWindow == NULL)
+// 	{
+// 		std::cerr << "Failed to create GLFW window" << std::endl;
+// 		glfwTerminate();
+// 		return false;
+// 	}
 
-	// Make the window's context the current one
-	glfwMakeContextCurrent(gWindow);
+// 	// Make the window's context the current one
+// 	glfwMakeContextCurrent(gWindow);
 
-	// Initialize GLEW
-	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK)
-	{
-		std::cerr << "Failed to initialize GLEW" << std::endl;
-		return false;
-	}
+// 	// Initialize GLEW
+// 	glewExperimental = GL_TRUE;
+// 	if (glewInit() != GLEW_OK)
+// 	{
+// 		std::cerr << "Failed to initialize GLEW" << std::endl;
+// 		return false;
+// 	}
 
-	// Set the required callback functions
-	glfwSetKeyCallback(gWindow, glfw_onKey);
-	glfwSetFramebufferSizeCallback(gWindow, glfw_onFramebufferSize);
-	glfwSetScrollCallback(gWindow, glfw_onMouseScroll);
+// 	// Set the required callback functions
+// 	glfwSetKeyCallback(gWindow, glfw_onKey);
+// 	glfwSetFramebufferSizeCallback(gWindow, glfw_onFramebufferSize);
+// 	glfwSetScrollCallback(gWindow, glfw_onMouseScroll);
 
-	// // Hides and grabs cursor, unlimited movement
-	// glfwSetInputMode(gWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
-	// glfwSetCursorPos(gWindow, gWindowWidth / 2.0, gWindowHeight / 2.0);
+// 	// // Hides and grabs cursor, unlimited movement
+// 	// glfwSetInputMode(gWindow, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+// 	// glfwSetCursorPos(gWindow, gWindowWidth / 2.0, gWindowHeight / 2.0);
 
-	glClearColor(gClearColor.r, gClearColor.g, gClearColor.b, gClearColor.a);
+// 	glClearColor(gClearColor.r, gClearColor.g, gClearColor.b, gClearColor.a);
 
-	// Define the viewport dimensions
-	glViewport(0, 0, gWindowWidth, gWindowHeight);
-	glEnable(GL_DEPTH_TEST);
+// 	// Define the viewport dimensions
+// 	glViewport(0, 0, gWindowWidth, gWindowHeight);
+// 	glEnable(GL_DEPTH_TEST);
 
-	return true;
-}
+// 	return true;
+// }
 
-//-----------------------------------------------------------------------------
-// Is called whenever a key is pressed/released via GLFW
-//-----------------------------------------------------------------------------
-void glfw_onKey(GLFWwindow *window, int key, int scancode, int action, int mode)
-{
-	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
-		glfwSetWindowShouldClose(window, GL_TRUE);
+// //-----------------------------------------------------------------------------
+// // Is called whenever a key is pressed/released via GLFW
+// //-----------------------------------------------------------------------------
+// void glfw_onKey(GLFWwindow *window, int key, int scancode, int action, int mode)
+// {
+// 	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+// 		glfwSetWindowShouldClose(window, GL_TRUE);
 
-	if (key == GLFW_KEY_F1 && action == GLFW_PRESS)
-	{
-		gWireframe = !gWireframe;
-		if (gWireframe)
-			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
-		else
-			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-	}
+// 	if (key == GLFW_KEY_F1 && action == GLFW_PRESS)
+// 	{
+// 		gWireframe = !gWireframe;
+// 		if (gWireframe)
+// 			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+// 		else
+// 			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+// 	}
 
-	if (key == GLFW_KEY_F && action == GLFW_PRESS)
-	{
-		// toggle the flashlight
-		gFlashlightOn = !gFlashlightOn;
-	}
-}
+// 	if (key == GLFW_KEY_F && action == GLFW_PRESS)
+// 	{
+// 		// toggle the flashlight
+// 		gFlashlightOn = !gFlashlightOn;
+// 	}
+// }
 
-//-----------------------------------------------------------------------------
-// Is called when the window is resized
-//-----------------------------------------------------------------------------
-void glfw_onFramebufferSize(GLFWwindow *window, int width, int height)
-{
-	gWindowWidth = width;
-	gWindowHeight = height;
-	glViewport(0, 0, gWindowWidth, gWindowHeight);
-}
+// //-----------------------------------------------------------------------------
+// // Is called when the window is resized
+// //-----------------------------------------------------------------------------
+// void glfw_onFramebufferSize(GLFWwindow *window, int width, int height)
+// {
+// 	gWindowWidth = width;
+// 	gWindowHeight = height;
+// 	glViewport(0, 0, gWindowWidth, gWindowHeight);
+// }
 
-//-----------------------------------------------------------------------------
-// Called by GLFW when the mouse wheel is rotated
-//-----------------------------------------------------------------------------
-void glfw_onMouseScroll(GLFWwindow *window, double deltaX, double deltaY)
-{
-	double fov = fpsCamera.getFOV() + deltaY * ZOOM_SENSITIVITY;
+// //-----------------------------------------------------------------------------
+// // Called by GLFW when the mouse wheel is rotated
+// //-----------------------------------------------------------------------------
+// void glfw_onMouseScroll(GLFWwindow *window, double deltaX, double deltaY)
+// {
+// 	double fov = fpsCamera.getFOV() + deltaY * ZOOM_SENSITIVITY;
 
-	fov = glm::clamp(fov, 1.0, 120.0);
+// 	fov = glm::clamp(fov, 1.0, 120.0);
 
-	fpsCamera.setFOV((float)fov);
-}
+// 	fpsCamera.setFOV((float)fov);
+// }
 
-//-----------------------------------------------------------------------------
-// Update stuff every frame
-//-----------------------------------------------------------------------------
-void update(double elapsedTime)
-{
-	// Camera orientation
-	double mouseX, mouseY;
+// //-----------------------------------------------------------------------------
+// // Update stuff every frame
+// //-----------------------------------------------------------------------------
+// void update(double elapsedTime)
+// {
+// 	// Camera orientation
+// 	double mouseX, mouseY;
 
-	// Get the current mouse cursor position delta
-	// glfwGetCursorPos(gWindow, &mouseX, &mouseY);
+// 	// Get the current mouse cursor position delta
+// 	// glfwGetCursorPos(gWindow, &mouseX, &mouseY);
 
-	// // Rotate the camera the difference in mouse distance from the center screen.  Multiply this delta by a speed scaler
-	// fpsCamera.rotate((float)(gWindowWidth / 2.0 - mouseX) * MOUSE_SENSITIVITY, (float)(gWindowHeight / 2.0 - mouseY) * MOUSE_SENSITIVITY);
+// 	// // Rotate the camera the difference in mouse distance from the center screen.  Multiply this delta by a speed scaler
+// 	// fpsCamera.rotate((float)(gWindowWidth / 2.0 - mouseX) * MOUSE_SENSITIVITY, (float)(gWindowHeight / 2.0 - mouseY) * MOUSE_SENSITIVITY);
 
-	// // Clamp mouse cursor to center of screen
-	// glfwSetCursorPos(gWindow, gWindowWidth / 2.0, gWindowHeight / 2.0);
+// 	// // Clamp mouse cursor to center of screen
+// 	// glfwSetCursorPos(gWindow, gWindowWidth / 2.0, gWindowHeight / 2.0);
 
-	// Camera FPS movement
+// 	// Camera FPS movement
 
-	// Forward/backward
-	if (glfwGetKey(gWindow, GLFW_KEY_W) == GLFW_PRESS)
-		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * fpsCamera.getLook());
-	else if (glfwGetKey(gWindow, GLFW_KEY_S) == GLFW_PRESS)
-		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * -fpsCamera.getLook());
+// 	// Forward/backward
+// 	if (glfwGetKey(gWindow, GLFW_KEY_W) == GLFW_PRESS)
+// 		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * fpsCamera.getLook());
+// 	else if (glfwGetKey(gWindow, GLFW_KEY_S) == GLFW_PRESS)
+// 		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * -fpsCamera.getLook());
 
-	// Strafe left/right
-	if (glfwGetKey(gWindow, GLFW_KEY_A) == GLFW_PRESS)
-		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * -fpsCamera.getRight());
-	else if (glfwGetKey(gWindow, GLFW_KEY_D) == GLFW_PRESS)
-		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * fpsCamera.getRight());
+// 	// Strafe left/right
+// 	if (glfwGetKey(gWindow, GLFW_KEY_A) == GLFW_PRESS)
+// 		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * -fpsCamera.getRight());
+// 	else if (glfwGetKey(gWindow, GLFW_KEY_D) == GLFW_PRESS)
+// 		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * fpsCamera.getRight());
 
-	// Up/down
-	if (glfwGetKey(gWindow, GLFW_KEY_Z) == GLFW_PRESS)
-		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * glm::vec3(0.0f, 1.0f, 0.0f));
-	else if (glfwGetKey(gWindow, GLFW_KEY_X) == GLFW_PRESS)
-		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * -glm::vec3(0.0f, 1.0f, 0.0f));
-}
+// 	// Up/down
+// 	if (glfwGetKey(gWindow, GLFW_KEY_Z) == GLFW_PRESS)
+// 		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * glm::vec3(0.0f, 1.0f, 0.0f));
+// 	else if (glfwGetKey(gWindow, GLFW_KEY_X) == GLFW_PRESS)
+// 		fpsCamera.move(MOVE_SPEED * (float)elapsedTime * -glm::vec3(0.0f, 1.0f, 0.0f));
+// }
 
-//-----------------------------------------------------------------------------
-// Code computes the average frames per second, and also the average time it takes
-// to render one frame.  These stats are appended to the window caption bar.
-//-----------------------------------------------------------------------------
-void showFPS(GLFWwindow *window)
-{
-	static double previousSeconds = 0.0;
-	static int frameCount = 0;
-	double elapsedSeconds;
-	double currentSeconds = glfwGetTime(); // returns number of seconds since GLFW started, as double float
+// //-----------------------------------------------------------------------------
+// // Code computes the average frames per second, and also the average time it takes
+// // to render one frame.  These stats are appended to the window caption bar.
+// //-----------------------------------------------------------------------------
+// void showFPS(GLFWwindow *window)
+// {
+// 	static double previousSeconds = 0.0;
+// 	static int frameCount = 0;
+// 	double elapsedSeconds;
+// 	double currentSeconds = glfwGetTime(); // returns number of seconds since GLFW started, as double float
 
-	elapsedSeconds = currentSeconds - previousSeconds;
+// 	elapsedSeconds = currentSeconds - previousSeconds;
 
-	// Limit text updates to 4 times per second
-	if (elapsedSeconds > 0.25)
-	{
-		previousSeconds = currentSeconds;
-		double fps = (double)frameCount / elapsedSeconds;
-		double msPerFrame = 1000.0 / fps;
+// 	// Limit text updates to 4 times per second
+// 	if (elapsedSeconds > 0.25)
+// 	{
+// 		previousSeconds = currentSeconds;
+// 		double fps = (double)frameCount / elapsedSeconds;
+// 		double msPerFrame = 1000.0 / fps;
 
-		// The C++ way of setting the window title
-		std::ostringstream outs;
-		outs.precision(3); // decimal places
-		outs << std::fixed
-			 << APP_TITLE << "    "
-			 << "FPS: " << fps << "    "
-			 << "Frame Time: " << msPerFrame << " (ms)";
-		glfwSetWindowTitle(window, outs.str().c_str());
+// 		// The C++ way of setting the window title
+// 		std::ostringstream outs;
+// 		outs.precision(3); // decimal places
+// 		outs << std::fixed
+// 			 << APP_TITLE << "    "
+// 			 << "FPS: " << fps << "    "
+// 			 << "Frame Time: " << msPerFrame << " (ms)";
+// 		glfwSetWindowTitle(window, outs.str().c_str());
 
-		// Reset for next average.
-		frameCount = 0;
-	}
+// 		// Reset for next average.
+// 		frameCount = 0;
+// 	}
 
-	frameCount++;
-}
-void mac_patch(GLFWwindow *window)
-{
-	if (glfwGetTime() > 3.0)
-	{
-		mac_moved = true;
-	}
-	// glfwGetTim
+// 	frameCount++;
+// }
+// void mac_patch(GLFWwindow *window)
+// {
+// 	if (glfwGetTime() > 3.0)
+// 	{
+// 		mac_moved = true;
+// 	}
+// 	// glfwGetTim
 
-	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_RELEASE && !mac_moved)
-	{
-		int x, y;
-		glfwGetWindowPos(window, &x, &y);
-		glfwSetWindowPos(window, ++x, y);
-	}
-	else
-	{
-		mac_moved = true;
-	}
-}
+// 	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_RELEASE && !mac_moved)
+// 	{
+// 		int x, y;
+// 		glfwGetWindowPos(window, &x, &y);
+// 		glfwSetWindowPos(window, ++x, y);
+// 	}
+// 	else
+// 	{
+// 		mac_moved = true;
+// 	}
+// }
+
+// float rand_num(int LO, int HI){
+// 	return (LO + static_cast <float> (rand()) /( static_cast <float> (RAND_MAX/(HI-LO))));
+
+// }
