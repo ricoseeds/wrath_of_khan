51a2ccbc8417ef89091ce54ef39deb5b89900ca5
ricoseeds <rghchaks73@gmail.com>
2019-04-09 10:35:07 +0530

    save

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ struct Particle
 };
 
 // Global Variables
-const char *APP_TITLE = "Introduction to Modern OpenGL - Multiple Lights";
+const char *APP_TITLE = "Wrath of khan";
 int gWindowWidth = 1024;
 int gWindowHeight = 768;
 GLFWwindow *gWindow = NULL;
@@ -48,12 +48,36 @@ static bool mac_moved = false;
 const int MaxParticles = 1000;
 Particle ParticlesContainer[MaxParticles];
 int LastUsedParticle = 0;
-
+static GLfloat *g_particule_position_size_data = new GLfloat[MaxParticles * 4];
+static GLubyte *g_particule_color_data = new GLubyte[MaxParticles * 4];
+static const GLfloat g_vertex_buffer_data[] = {
+	-0.5f,
+	-0.5f,
+	0.0f,
+	0.5f,
+	-0.5f,
+	0.0f,
+	-0.5f,
+	0.5f,
+	0.0f,
+	0.5f,
+	0.5f,
+	0.0f,
+};
+GLuint billboard_vertex_buffer;
+GLuint particles_position_buffer;
+GLuint particles_color_buffer;
+GLuint TextureID;
 FPSCamera fpsCamera(glm::vec3(0.0f, 3.5f, 10.0f));
 const double ZOOM_SENSITIVITY = -3.0;
 const float MOVE_SPEED = 5.0; // units per second
 const float MOUSE_SENSITIVITY = 0.1f;
-
+GLuint CameraRight_worldspace_ID; //= glGetUniformLocation(programID, "CameraRight_worldspace");
+GLuint CameraUp_worldspace_ID;	//= glGetUniformLocation(programID, "CameraUp_worldspace");
+GLuint ViewProjMatrixID;		  //= glGetUniformLocation(programID, "VP");
+GLuint Texture;
+GLuint VertexArrayID;
+GLuint programID;
 // Function prototypes
 void glfw_onKey(GLFWwindow *window, int key, int scancode, int action, int mode);
 void glfw_onFramebufferSize(GLFWwindow *window, int width, int height);
@@ -118,7 +142,7 @@ int main()
 
 	// Model positions
 	glm::vec3 modelPos[] = {
-		glm::vec3(0.0f, 0.0f, 0.0f), // barrel
+		glm::vec3(-5.0f, 0.0f, 0.0f), // barrel
 
 	};
 
@@ -137,9 +161,40 @@ int main()
 	double lastTime = glfwGetTime();
 
 	// Rendering loop
+	// glDepthFunc(GL_LESS);
+	//shadervarss
+	glGenVertexArrays(1, &VertexArrayID);
+	glBindVertexArray(VertexArrayID);
+	programID = LoadShaders("shaders/Particle.vertexshader", "shaders/Particle.fragmentshader");
+	CameraRight_worldspace_ID = glGetUniformLocation(programID, "CameraRight_worldspace");
+	CameraUp_worldspace_ID = glGetUniformLocation(programID, "CameraUp_worldspace");
+	ViewProjMatrixID = glGetUniformLocation(programID, "VP");
+	TextureID = glGetUniformLocation(programID, "myTextureSampler");
+	for (int i = 0; i < MaxParticles; i++)
+	{
+		ParticlesContainer[i].life = -1.0f;
+		ParticlesContainer[i].cameradistance = -1.0f;
+	}
+	Texture = loadDDS("textures/particle.DDS");
+	glGenBuffers(1, &billboard_vertex_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, billboard_vertex_buffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
+
+	glGenBuffers(1, &particles_position_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
+	// Initialize with empty (NULL) buffer : it will be updated later, each frame.
+	glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW);
+
+	// The VBO containing the colors of the particles
+	glGenBuffers(1, &particles_color_buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, particles_color_buffer);
+	// Initialize with empty (NULL) buffer : it will be updated later, each frame.
+	glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW);
+
 	while (!glfwWindowShouldClose(gWindow))
 	{
 		showFPS(gWindow);
+		glUseProgram(programID);
 
 		double currentTime = glfwGetTime();
 		double deltaTime = currentTime - lastTime;
@@ -155,7 +210,7 @@ int main()
 
 		// Create the View matrix
 		view = fpsCamera.getViewMatrix();
-
+		glm::mat4 ViewMatrix = view;
 		// Create the projection matrix
 		projection = glm::perspective(glm::radians(fpsCamera.getFOV()), (float)gWindowWidth / (float)gWindowHeight, 0.1f, 200.0f);
 
@@ -164,54 +219,171 @@ int main()
 		viewPos.x = fpsCamera.getPosition().x;
 		viewPos.y = fpsCamera.getPosition().y;
 		viewPos.z = fpsCamera.getPosition().z;
+		glm::vec3 CameraPosition(glm::inverse(view)[3]);
+		glm::mat4 ViewProjectionMatrix = projection * view;
+		//BEGIN PARTICLES
+		int newparticles = (int)(deltaTime * 10000.0);
+		if (newparticles > (int)(0.016f * 10000.0))
+			newparticles = (int)(0.016f * 10000.0);
 
+		for (int i = 0; i < newparticles; i++)
+		{
+			int particleIndex = FindUnusedParticle();
+			ParticlesContainer[particleIndex].life = 1.0f; // This particle will live 5 seconds.
+			ParticlesContainer[particleIndex].pos = glm::vec3(0, 0, -11.0f);
+
+			float spread = 1.5f;
+			glm::vec3 maindir = glm::vec3(0.0f, 10.0f, 0.0f);
+			// Very bad way to generate a random direction;
+			// See for instance http://stackoverflow.com/questions/5408276/python-uniform-spherical-distribution instead,
+			// combined with some user-controlled parameters (main direction, spread, etc)
+			glm::vec3 randomdir = glm::vec3(
+				(rand() % 2000 - 1000.0f) / 1000.0f,
+				(rand() % 2000 - 1000.0f) / 1000.0f,
+				(rand() % 2000 - 1000.0f) / 1000.0f);
+
+			ParticlesContainer[particleIndex].speed = maindir + randomdir * spread;
+
+			// Very bad way to generate a random color
+			ParticlesContainer[particleIndex].r = rand() % 256;
+			ParticlesContainer[particleIndex].g = rand() % 256;
+			ParticlesContainer[particleIndex].b = rand() % 256;
+			ParticlesContainer[particleIndex].a = (rand() % 256) / 3;
+
+			ParticlesContainer[particleIndex].size = (rand() % 1000) / 2000.0f + 0.1f;
+		}
+		// Simulate all particles
+		int ParticlesCount = 0;
+		for (int i = 0; i < MaxParticles; i++)
+		{
+
+			Particle &p = ParticlesContainer[i]; // shortcut
+
+			if (p.life > 0.0f)
+			{
+
+				// Decrease life
+				p.life -= deltaTime;
+				if (p.life > 0.0f)
+				{
+
+					// Simulate simple physics : gravity only, no collisions
+					p.speed += glm::vec3(0.0f, -9.81f, 0.0f) * (float)deltaTime * 0.5f;
+					p.pos += p.speed * (float)deltaTime;
+					// if (i == 1)
+					// {
+					// 	// std::cout << glm::to_string(p.pos) << std::endl;
+					// }
+
+					// std::cout << glm::to_string(p.pos) << std::endl;
+					p.cameradistance = glm::length2(p.pos - CameraPosition);
+					//ParticlesContainer[i].pos += glm::vec3(0.0f,10.0f, 0.0f) * (float)delta;
+
+					// Fill the GPU buffer
+					g_particule_position_size_data[4 * ParticlesCount + 0] = p.pos.x;
+					g_particule_position_size_data[4 * ParticlesCount + 1] = p.pos.y;
+					g_particule_position_size_data[4 * ParticlesCount + 2] = p.pos.z;
+
+					g_particule_position_size_data[4 * ParticlesCount + 3] = p.size;
+
+					g_particule_color_data[4 * ParticlesCount + 0] = p.r;
+					g_particule_color_data[4 * ParticlesCount + 1] = p.g;
+					g_particule_color_data[4 * ParticlesCount + 2] = p.b;
+					g_particule_color_data[4 * ParticlesCount + 3] = p.a;
+				}
+				else
+				{
+					// Particles that just died will be put at the end of the buffer in SortParticles();
+					p.cameradistance = -1.0f;
+				}
+
+				ParticlesCount++;
+			}
+		}
+		SortParticles();
+		glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
+		glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
+		glBufferSubData(GL_ARRAY_BUFFER, 0, ParticlesCount * sizeof(GLfloat) * 4, g_particule_position_size_data);
+
+		glBindBuffer(GL_ARRAY_BUFFER, particles_color_buffer);
+		glBufferData(GL_ARRAY_BUFFER, MaxParticles * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf. See above link for details.
+		glBufferSubData(GL_ARRAY_BUFFER, 0, ParticlesCount * sizeof(GLubyte) * 4, g_particule_color_data);
+
+		glEnable(GL_BLEND);
+		glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+		// Use our shader
+		glUseProgram(programID);
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, Texture);
+		// Set our "myTextureSampler" sampler to use Texture Unit 0
+		glUniform1i(TextureID, 0);
+		glUniform3f(CameraRight_worldspace_ID, ViewMatrix[0][0], ViewMatrix[1][0], ViewMatrix[2][0]);
+		glUniform3f(CameraUp_worldspace_ID, ViewMatrix[0][1], ViewMatrix[1][1], ViewMatrix[2][1]);
+
+		glUniformMatrix4fv(ViewProjMatrixID, 1, GL_FALSE, &ViewProjectionMatrix[0][0]);
+
+		// 1rst attribute buffer : vertices
+		glEnableVertexAttribArray(0);
+		glBindBuffer(GL_ARRAY_BUFFER, billboard_vertex_buffer);
+		glVertexAttribPointer(
+			0,		  // attribute. No particular reason for 0, but must match the layout in the shader.
+			3,		  // size
+			GL_FLOAT, // type
+			GL_FALSE, // normalized?
+			0,		  // stride
+			(void *)0 // array buffer offset
+		);
+		glEnableVertexAttribArray(1);
+		glBindBuffer(GL_ARRAY_BUFFER, particles_position_buffer);
+		glVertexAttribPointer(
+			1,		  // attribute. No particular reason for 1, but must match the layout in the shader.
+			4,		  // size : x + y + z + size => 4
+			GL_FLOAT, // type
+			GL_FALSE, // normalized?
+			0,		  // stride
+			(void *)0 // array buffer offset
+		);
+
+		// 3rd attribute buffer : particles' colors
+		glEnableVertexAttribArray(2);
+		glBindBuffer(GL_ARRAY_BUFFER, particles_color_buffer);
+		glVertexAttribPointer(
+			2,				  // attribute. No particular reason for 1, but must match the layout in the shader.
+			4,				  // size : r + g + b + a => 4
+			GL_UNSIGNED_BYTE, // type
+			GL_TRUE,		  // normalized?    *** YES, this means that the unsigned char[4] will be accessible with a vec4 (floats) in the shader ***
+			0,				  // stride
+			(void *)0		  // array buffer offset
+		);
+
+		// These functions are specific to glDrawArrays*Instanced*.
+		// The first parameter is the attribute buffer we're talking about.
+		// The second parameter is the "rate at which generic vertex attributes advance when rendering multiple instances"
+		// http://www.opengl.org/sdk/docs/man/xhtml/glVertexAttribDivisor.xml
+		glVertexAttribDivisor(0, 0); // particles vertices : always reuse the same 4 vertices -> 0
+		glVertexAttribDivisor(1, 1); // positions : one per quad (its center)                 -> 1
+		glVertexAttribDivisor(2, 1); // color : one per quad                                  -> 1
+		glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, ParticlesCount);
+
+		glDisableVertexAttribArray(0);
+		glDisableVertexAttribArray(1);
+		glDisableVertexAttribArray(2);
+		//END OF PARTICLES
 		// Must be called BEFORE setting uniforms because setting uniforms is done
 		// on the currently active shader program.
+
 		lightingShader.use();
 		lightingShader.setUniform("model", glm::mat4(1.0)); // do not need to translate the models so just send the identity matrix
 		lightingShader.setUniform("view", view);
 		lightingShader.setUniform("projection", projection);
 		lightingShader.setUniform("viewPos", viewPos);
 
-		// Directional light
+		// // Directional light
 		lightingShader.setUniform("sunLight.direction", glm::vec3(0.0f, -0.9f, -0.17f));
 		lightingShader.setUniform("sunLight.ambient", glm::vec3(0.1f, 0.1f, 0.1f));
 		lightingShader.setUniform("sunLight.diffuse", glm::vec3(0.1f, 0.1f, 0.1f)); // dark
 		lightingShader.setUniform("sunLight.specular", glm::vec3(0.1f, 0.1f, 0.1f));
-
-		// // Point Light 1
-		// lightingShader.setUniform("pointLights[0].ambient", glm::vec3(0.2f, 0.2f, 0.2f));
-		// lightingShader.setUniform("pointLights[0].diffuse", glm::vec3(0.0f, 1.0f, 0.1f)); // green-ish light
-		// lightingShader.setUniform("pointLights[0].specular", glm::vec3(1.0f, 1.0f, 1.0f));
-		// lightingShader.setUniform("pointLights[0].position", pointLightPos[0]);
-		// lightingShader.setUniform("pointLights[0].constant", 1.0f);
-		// lightingShader.setUniform("pointLights[0].linear", 0.0022f);
-		// lightingShader.setUniform("pointLights[0].exponent", 0.0020f);
-
-		// // Point Light 2
-		// lightingShader.setUniform("pointLights[1].ambient", glm::vec3(0.2f, 0.2f, 0.2f));
-		// lightingShader.setUniform("pointLights[1].diffuse", glm::vec3(1.0f, 0.1f, 0.0f)); // red-ish light
-		// lightingShader.setUniform("pointLights[1].specular", glm::vec3(1.0f, 1.0f, 1.0f));
-		// lightingShader.setUniform("pointLights[1].position", pointLightPos[1]);
-		// lightingShader.setUniform("pointLights[1].constant", 1.0f);
-		// lightingShader.setUniform("pointLights[1].linear", 0.0022f);
-		// lightingShader.setUniform("pointLights[1].exponent", 0.0020f);
-
-		// // Point Light 3
-		// lightingShader.setUniform("pointLights[2].ambient", glm::vec3(0.2f, 0.2f, 0.2f));
-		// lightingShader.setUniform("pointLights[2].diffuse", glm::vec3(0.0f, 0.1f, 1.0f)); // blue-ish light
-		// lightingShader.setUniform("pointLights[2].specular", glm::vec3(1.0f, 1.0f, 1.0f));
-		// lightingShader.setUniform("pointLights[2].position", pointLightPos[2]);
-		// lightingShader.setUniform("pointLights[2].constant", 1.0f);
-		// lightingShader.setUniform("pointLights[2].linear", 0.0022f);
-		// lightingShader.setUniform("pointLights[2].exponent", 0.0020f);
-
-		// Spot light
-		glm::vec3 spotlightPos = fpsCamera.getPosition();
-		std::cout << "POS : " << glm::to_string(fpsCamera.getPosition()) << std::endl;
-		std::cout << "LOOK : " << glm::to_string(fpsCamera.getLook()) << std::endl;
-		// offset the flash light down a little
-		spotlightPos.y -= 0.5f;
 
 		lightingShader.setUniform("spotLight.ambient", glm::vec3(0.1f, 0.1f, 0.1f));
 		lightingShader.setUniform("spotLight.diffuse", glm::vec3(0.8f, 0.8f, 0.8f));
@@ -226,13 +398,13 @@ int main()
 		lightingShader.setUniform("spotLight.on", gFlashlightOn);
 
 		// Render the scene
-		for (int i = 0; i < numModels; i++)
+		for (int i = 0; i < 1; i++)
 		{
 			model = glm::translate(glm::mat4(1.0), modelPos[i]) * glm::scale(glm::mat4(1.0), modelScale[i]); // * glm::rotate(glm::mat4(1.0), glm::radians((float)(glfwGetTime() * 100.0f)), glm::vec3(1.0f, 0.0f, 0.0f));
 			;
 			lightingShader.setUniform("model", model);
 
-			// Set material properties
+			// 	// Set material properties
 			lightingShader.setUniform("material.ambient", glm::vec3(0.1f, 0.1f, 0.1f));
 			lightingShader.setUniformSampler("material.diffuseMap", 0);
 			lightingShader.setUniform("material.specular", glm::vec3(0.8f, 0.8f, 0.8f));
@@ -308,6 +480,7 @@ bool initOpenGL()
 	// Define the viewport dimensions
 	glViewport(0, 0, gWindowWidth, gWindowHeight);
 	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
 
 	return true;
 }
